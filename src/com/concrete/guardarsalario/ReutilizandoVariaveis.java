09560ac3e05aadb031121c3845d3ef52943cf825
btoledo90 <brunotq19@hotmail.com>
2021-06-18 17:12:48 -0300

    Adicionando classe guardar salario

---
+++ src/com/concrete/guardarsalario/ReutilizandoVariaveis.java
@@ -0,0 +1,19 @@
+package com.concrete.guardarsalario;
+
+import java.util.Scanner;
+
+public class ReutilizandoVariaveis {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe o salario: ");
+		double salario = sc.nextDouble();
+		double resultado = salario * 0.15;
+		System.out.println("30% do salario é: " + resultado);
+		resultado = salario * 0.15;
+		System.out.println("15% do salario é: " + resultado);
+		resultado = salario * 0.05;
+		System.out.println("5% do salario é: " + resultado);
+	}
+
+}
