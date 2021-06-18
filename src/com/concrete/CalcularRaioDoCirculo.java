02420fcbbbc6f8fd691beff1fb8a21f4933e358b
btoledo90 <brunotq19@hotmail.com>
2021-06-18 00:50:47 -0300

    Add class calculate circle radius

--- src/com/concrete/CalcularRaioDoCirculo.java
+++ src/com/concrete/CalcularRaioDoCirculo.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CalcularRaioDoCirculo {
 
     public static void main(String[] args) {
+    	
         Scanner scan = new Scanner(System.in);
 
         double raio, area;
