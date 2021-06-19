25ccb8c10e30eae918d1c7631575c0a4b82ca55c
btoledo90 <brunotq19@hotmail.com>
2021-06-19 20:30:30 -0300

    Fahrenheit para Celsius

---
+++ src/com/concrete/ConversaoCelsius.java
@@ -0,0 +1,18 @@
+package com.concrete;
+
+import java.util.Scanner;
+
+public class ConversaoCelsius {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe a temperatura em Fahrenheit");
+		double fahrenheit = sc.nextDouble();
+
+		System.out.println("A temperatura em Fahrenheit é: " + ((fahrenheit - 32) * 5) / 9);
+
+		sc.close();
+	}
+
+}
