0b6871c3da685c8a02f17ce7a0808b9e91554db8
btoledo90 <brunotq19@hotmail.com>
2021-06-19 19:18:04 -0300

    Calculo do salario

---
+++ src/com/concrete/CalculoDoSalario.java
@@ -0,0 +1,29 @@
+package com.concrete;
+
+import java.util.Scanner;
+
+public class CalculoDoSalario {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe seu salário base: ");
+		double salarioBase = sc.nextDouble();
+		System.out.println("Informe as horas trabalhadas: ");
+		int horasTrabalhadas = sc.nextInt();
+		System.out.println("Informe o valor das horas trabalhadas: ");
+		double valorHoraDeTrabalho = sc.nextDouble();
+		double totalDeDescontos;
+		System.out.println("Informe o percentual de desconto: ");
+		double percentualDeDesconto = sc.nextDouble();
+		double salarioLiquido;
+
+		salarioBase = horasTrabalhadas * valorHoraDeTrabalho;
+		totalDeDescontos = (percentualDeDesconto / 100) * salarioBase;
+		salarioLiquido = salarioBase - percentualDeDesconto;
+
+		System.out.println("A base do salário é: " + salarioBase + " e o salário liquído é: " + salarioLiquido);
+
+		sc.close();
+	}
+
+}
