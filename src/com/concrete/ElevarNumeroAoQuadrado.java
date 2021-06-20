a835c150051615db5d0a2575d7c082a888b0cb76
btoledo90 <brunotq19@hotmail.com>
2021-06-20 19:55:39 -0300

    Add classe Elevar Numero

---
+++ src/com/concrete/ElevarNumeroAoQuadrado.java
@@ -0,0 +1,29 @@
+package com.concrete;
+
+import java.util.Scanner;
+
+public class ElevarNumeroAoQuadrado {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int numero;
+		double resultado;
+		double resultadoCubo;
+		double resultadoDecima;
+
+		System.out.println("Digite um número que elevaremos ao quadrado, cubo e a décima: ");
+		numero = sc.nextInt();
+
+		resultado = (Math.pow(numero, 2));
+		resultadoCubo = (Math.pow(numero, 3));
+		resultadoDecima = (Math.pow(numero, 10));
+
+		System.out.println("O número elevado ao quadrado é: " + resultado);
+		System.out.println("O número elevado ao cubo é: " + resultadoCubo);
+		System.out.println("O número elevado a décima é: " + resultadoDecima);
+
+		sc.close();
+
+	}
+
+}
