1860a041b8745400107584bf152972a7941ed85f
btoledo90 <brunotq19@hotmail.com>
2021-06-21 11:53:11 -0300

    Add classe Antecessor e Sucessor

---
+++ src/com/concrete/SucessorAntecessor.java
@@ -0,0 +1,24 @@
+package com.concrete;
+
+import java.util.Scanner;
+
+public class SucessorAntecessor {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int numero, antecessor, sucessor;
+
+		System.out.println("Digite um número: ");
+		numero = sc.nextInt();
+
+		antecessor = numero - 1;
+		sucessor = numero + 1;
+
+		System.out.println("O antecessor do numero: " + numero + " é o " + antecessor + " e o sucessor" + " do número: "
+				+ numero + " é o " + sucessor);
+
+		sc.close();
+
+	}
+}
