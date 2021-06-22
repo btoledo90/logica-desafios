8f526049da120ac19ace1756db2d36a205b56e55
btoledo90 <brunotq19@hotmail.com>
2021-06-22 17:25:29 -0300

    Add classe Tabuada

---
+++ src/com/concrete/lacosrepeticao/Tabuada.java
@@ -0,0 +1,11 @@
+package com.concrete.lacosrepeticao;
+
+public class Tabuada {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(8 * i);
+		}
+
+	}
+}
