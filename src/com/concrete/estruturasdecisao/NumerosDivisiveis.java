db07e7c99e356910423dd83f5f4c2d8a6257999b
btoledo90 <brunotq19@hotmail.com>
2021-06-22 15:40:11 -0300

    Update

--- src/com/concrete/estruturasdecisao/NumerosDivisiveis.java
+++ src/com/concrete/estruturasdecisao/NumerosDivisiveis.java
@@ -7,16 +7,32 @@ public class NumerosDivisiveis {
 
 		Scanner sc = new Scanner(System.in);
 
-		int num1, num2, num3, num4, num5, div2, div3;
+		int num1, num2, num3, num4, num5,div1, div2;
 
-		System.out.println("Digite o primeiro número:");
+		System.out.println("Digite o primeiro número: ");
 		num1 = sc.nextInt();
+		System.out.println("Digite o segundo número: ");
+		num2 = sc.nextInt();
+		System.out.println("Digite o terceiro número: ");
+		num3 = sc.nextInt();
+		System.out.println("Digite o primeiro número:");
+		num4 = sc.nextInt();
+		System.out.println("Digite o primeiro número:");
+		num5 = sc.nextInt();
+		
+		div1 = num1 % 1;
+		div2 = num2 % 2;
 
-		div2 = num1 % 2;
-		div3 = num1 % 3;
-
-		if (div2 == 0 && div3 == 0) {
+		if (num1 % 2 == 0 && num1 % 3 == 0) {
 			System.out.println(num1 + " é divisível por 2 e por 3");
+		} else if (num2 % 2 == 0 && num2 % 3 == 0) {
+			System.out.println(num2 + " é divisível por 2 e por 3");
+		} else if (num3 % 2 == 0 && num3 % 3 == 0) {
+			System.out.println(num3 + " é divisível por 2 e por 3");
+		} else if (num4 % 2 == 0 && num4 % 3 == 0) {
+			System.out.println(num4 + " é divisível por 2 e por 3");
+		} else if (num5 % 2 == 0 && num5 % 3 == 0) {
+			System.out.println(num5 + " é divisível por 2 e por 3");
 		} else {
 			System.out.println(num1 + " não é divisível  por 2 e por 3");
 		}
