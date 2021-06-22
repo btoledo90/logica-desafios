bed5ed83ac83d28a03a2307baac72c6fcf8d56e8
btoledo90 <brunotq19@hotmail.com>
2021-06-22 17:12:53 -0300

    Add classe Quadrado do numero

---
+++ src/com/concrete/lacosrepeticao/QuadradoDosNumeros.java
@@ -0,0 +1,13 @@
+package com.concrete.lacosrepeticao;
+
+public class QuadradoDosNumeros {
+	public static void main(String[] args) {
+
+		for (int i = 15; i <= 200; i++) {
+
+			System.out.println(i * i);
+
+		}
+
+	}
+}
